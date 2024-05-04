dc3b9a33f748af101c320db68587659d2a55da08
Errechydy <57603911+Errechydy@users.noreply.github.com>
2024-05-04 15:57:39 +0100

    Update el_screen_class.dart

--- lib/src/el_screen_class.dart
+++ lib/src/el_screen_class.dart
@@ -1,5 +1,8 @@
+/// Set the name and the break point of this sreen
 class ElScreen<T> {
+  /// Choose a [name] of any type [T] you prefer for this breakpoint
   final T name;
+  /// Breakpoint of type int
   final int breakpoint;
 
   ElScreen({
